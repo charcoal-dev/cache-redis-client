d04134d36032fa518dc6e0e5aff102121ee977b8
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2023-08-17 00:28:57 +0400

    Redis exceptions

---
+++ src/Exception/RedisConnectionException.php
@@ -0,0 +1,26 @@
+<?php
+/*
+ * This file is a part of "charcoal-dev/cache-redis-client" package.
+ * https://github.com/charcoal-dev/cache-redis-client
+ *
+ * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/charcoal-dev/cache-redis-client/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\Cache\Drivers\Exception;
+
+use Charcoal\Cache\Exception\CacheDriverConnectionException;
+
+/**
+ * Class RedisConnectionException
+ * @package Charcoal\Cache\Drivers\Exception
+ */
+class RedisConnectionException extends CacheDriverConnectionException
+{
+}
+
---
+++ src/Exception/RedisOpException.php
@@ -0,0 +1,25 @@
+<?php
+/*
+ * This file is a part of "charcoal-dev/cache-redis-client" package.
+ * https://github.com/charcoal-dev/cache-redis-client
+ *
+ * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code or visit following link:
+ * https://github.com/charcoal-dev/cache-redis-client/blob/master/LICENSE
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\Cache\Drivers\Exception;
+
+use Charcoal\Cache\Exception\CacheDriverOpException;
+
+/**
+ * Class RedisOpException
+ * @package Charcoal\Cache\Drivers\Exception
+ */
+class RedisOpException extends CacheDriverOpException
+{
+}
