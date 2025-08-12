37b3e2caf4b68998ccf0d396f9a1582959d1103d
M. Furqan Siddiqui <hello@furqansiddiqui.com>
2025-08-12 09:32:06 +0500

    update exception headers

--- src/Exception/RedisOpException.php
+++ src/Exception/RedisOpException.php
@@ -1,13 +1,7 @@
 <?php
-/*
- * This file is a part of "charcoal-dev/cache-redis-client" package.
- * https://github.com/charcoal-dev/cache-redis-client
- *
- * Copyright (c) Furqan A. Siddiqui <hello@furqansiddiqui.com>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code or visit following link:
- * https://github.com/charcoal-dev/cache-redis-client/blob/master/LICENSE
+/**
+ * Part of the "charcoal-dev/cache-redis-client" package.
+ * @link https://github.com/charcoal-dev/cache-redis-client
  */
 
 declare(strict_types=1);
