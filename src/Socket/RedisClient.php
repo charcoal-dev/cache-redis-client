d537d004d179d5fd272c7eadf77aa117005ba471
Furqan Siddiqui <hello@furqansiddiqui.com>
2025-09-05 04:16:26 +0500

    feat: add class alias for RedisClient to RedisClientV2
    - Defined `RedisClient` as an alias for `RedisClientV2` to ensure backward compatibility.

---
+++ src/Socket/RedisClient.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Part of the "charcoal-dev/cache-redis-client" package.
+ * @link https://github.com/charcoal-dev/cache-redis-client
+ */
+
+declare(strict_types=1);
+
+namespace Charcoal\Cache\Adapters\Redis\Socket;
+
+class_alias(RedisClientV2::class, RedisClient::class, true);
